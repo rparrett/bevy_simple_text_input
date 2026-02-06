531682091d701cf8cbf6312a8c88cb2824254b14
Cyannide <cyannide@nullhost.net>
2026-02-05 19:53:22 -0600

    Fix inverted behavior of `hide_on_focus` (#113)
    * Fixed hide_on_focus negation.

--- src/lib.rs
+++ src/lib.rs
@@ -276,6 +276,7 @@ pub struct TextInputPlaceholder {
     ///
     pub hide_on_focus: bool,
 }
+
 impl Default for TextInputPlaceholder {
     fn default() -> Self {
         Self {
@@ -650,7 +651,7 @@ fn create(
             .unwrap_or_else(|| placeholder_color(&color.0));
 
         let placeholder_visible =
-            (placeholder.hide_on_focus || inactive.0) && text_input.0.is_empty();
+            (!placeholder.hide_on_focus || inactive.0) && text_input.0.is_empty();
 
         let placeholder_text = commands
             .spawn((
@@ -799,7 +800,7 @@ fn show_hide_placeholder(
         let mut iter = vis_query.iter_many_mut(children);
         while let Some(mut inner_vis) = iter.fetch_next() {
             inner_vis.set_if_neq(
-                if text.0.is_empty() && (placeholder.hide_on_focus || inactive.0) {
+                if text.0.is_empty() && (!placeholder.hide_on_focus || inactive.0) {
                     Visibility::Inherited
                 } else {
                     Visibility::Hidden
